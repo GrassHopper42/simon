06d1fc2e0a43fdfe09448835f2d1d938d7412653
grasshopper42 <solite4686@gmail.com>
2022-12-09 18:55:02 +0900

    :tada:init

---
+++ src/main/java/com/simon/SimonApplication.java
@@ -0,0 +1,13 @@
+package com.simon;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SimonApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SimonApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/simon/SimonApplicationTests.java
@@ -0,0 +1,13 @@
+package com.simon;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SimonApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
