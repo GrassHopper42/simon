6827f645b309942aaa300a08b406934146bf3f1e
grasshopper42 <solite4686@gmail.com>
2023-07-28 10:45:40 +0900

    :tada: init

--- src/test/java/dau/azit/simon/SimonApplicationTests.java
+++ src/test/java/dau/azit/simon/SimonApplicationTests.java
@@ -1,4 +1,4 @@
-package com.simon;
+package dau.azit.simon;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -6,8 +6,8 @@ import org.springframework.boot.test.context.SpringBootTest;
 @SpringBootTest
 class SimonApplicationTests {
 
-    @Test
-    void contextLoads() {
-    }
+	@Test
+	void contextLoads() {
+	}
 
 }
