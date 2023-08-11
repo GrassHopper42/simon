f84bd9ae8a65700b665db6458e1592589186b238
constwon111 <constwon111@gmail.com>
2023-08-11 18:02:12 +0900

    #11/feat : Customer 엔티티 구현 - Order와 OneToMany 관계

---
+++ src/main/java/dau/azit/simon/customer/domain/Customer.java
@@ -0,0 +1,51 @@
+package dau.azit.simon.customer.domain;
+
+import dau.azit.simon.order.domain.Order;
+import jakarta.persistence.*;
+import lombok.Getter;
+
+import java.util.List;
+import java.util.UUID;
+
+@Getter()
+@Entity(name = "customers")
+public class Customer {
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+
+	@Column(nullable = false)
+	private UUID uid;
+
+	@Column(nullable = false)
+	String name;
+
+	String companyName;
+
+	String companyAddress;
+
+	@Column(nullable = false)
+	String contact;
+
+	@OneToMany()
+	@Column(nullable = false)
+	List<Order> orders;
+
+	String companyRepresentative;
+
+	public Customer() {
+	}
+
+	static public Customer createCustomer(String name, String contact, String companyName, String companyAddress, String companyRepresentative) {
+		return new Customer(name, contact, companyName, companyAddress, companyRepresentative);
+	}
+
+	private Customer(String name, String contact, String companyName, String companyAddress, String companyRepresentative) {
+		this.uid = UUID.randomUUID();
+		this.name = name;
+		this.companyName = companyName;
+		this.companyAddress = companyAddress;
+		this.contact = contact;
+		this.companyRepresentative = companyRepresentative;
+	}
+}
