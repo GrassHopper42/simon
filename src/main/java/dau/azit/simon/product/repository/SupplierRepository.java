c61d33c4f44f8334bd2ae7be34d88d742a6e9633
grasshopper42 <solite4686@gmail.com>
2023-09-07 16:37:29 +0900

    fix : 공급처 오류 수정 및 QueryDsl 구현

--- src/main/java/dau/azit/simon/product/repository/SupplierRepository.java
+++ src/main/java/dau/azit/simon/product/repository/SupplierRepository.java
@@ -3,5 +3,5 @@ package dau.azit.simon.product.repository;
 import dau.azit.simon.product.domain.Supplier;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface SupplierJpaRepository extends JpaRepository<Supplier, Long> {
+public interface SupplierRepository extends JpaRepository<Supplier, Long> {
 }
